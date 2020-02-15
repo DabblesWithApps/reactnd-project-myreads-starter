ba15ce24c4cdd1f585933dd598a68b601c6a083a
Shea Mentges <shea.mentges@gmail.com>
2020-02-15 00:16:07 -0800

    fix: no longer access imageLinks if it is undefined

--- src/Book.js
+++ src/Book.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ShelfChanger from './ShelfChanger'
 
 function Book(props) {
+    const backgroundImage = props.book.imageLinks && props.book.imageLinks.smallThumbnail ? props.book.imageLinks.smallThumbnail : null
     return (
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${props.book.imageLinks.smallThumbnail}")` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${backgroundImage}")` }}></div>
                 <ShelfChanger moveBook={props.moveBook} book={props.book} />
             </div>
             <div className="book-title">{props.book.title}</div>
